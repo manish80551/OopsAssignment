f8ea6d050fc9edd3c1b9a1682a02d0502c7a7b03
Manish M <manish.kumar@kalpitatechnologies.com>
2022-08-04 14:38:12 +0530

    oops assignment 1

---
+++ Basic_of_Oops/MultipleIn.cs
@@ -0,0 +1,33 @@
+﻿using Basic_of_Oops;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_of_Oops
+{
+    internal class MultipleIn
+    {
+        void show()
+        {
+            Console.WriteLine("1 st class Method");
+        }
+    }
+}
+
+class Second
+{
+    void show()
+    {
+        void show()
+        {
+            Console.WriteLine("2nd Class Method");       }
+    }
+}
+
+class MultipleTest : MultipleIn
+{
+    
+   
+}
---
+++ Basic_of_Oops/Program.cs
@@ -0,0 +1,54 @@
+﻿using Basic_of_Oops;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_of_Oops
+{
+   public class Employee_Details
+    {
+        int emp_id;
+        string emp_name;
+        double salary;
+        string address;
+        double contact_num;
+        string blood_group;
+
+        public void employee_Info(int emp_id,string emp_name,double salary,string address,double contact_num, string blood_group)
+        {
+            this.emp_id = emp_id;
+            this.emp_name = emp_name;
+            this.salary = salary;
+            this.address = address;
+            this.contact_num = contact_num;
+            this.blood_group = blood_group;
+        }
+
+        public void get_Details()
+        {
+            Console.WriteLine("Name of the employee is : {0}", emp_name);
+            Console.WriteLine("Id number of the Employee : {0}", emp_id);
+            Console.WriteLine("Employee Salary is : {0}", salary);
+            Console.WriteLine("Employee Adress is : {0}", address);
+            Console.WriteLine("Employee Contact number is : {0}", contact_num);
+            Console.WriteLine("Employee BloodGroup is : "+blood_group);
+           
+        }
+    } 
+    
+}
+
+class Employee_Tester
+{
+    static void Main(string[] ar)
+    {
+        Employee_Details details = new Employee_Details();
+        details.employee_Info(123, "Sagar", 7854699, "Belgum", 7894645,"O +ve");
+        details.get_Details();
+        Console.ReadLine();
+
+    }
+
+}
